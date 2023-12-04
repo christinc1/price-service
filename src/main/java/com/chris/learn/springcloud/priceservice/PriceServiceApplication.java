44e07e237c8d4035225b530efb7f6fbf1155dd60
christin_chacko_qb <christin@qburst.com>
2023-12-05 00:07:52 +0530

    add eureka client properties and dependenices

--- src/main/java/com/chris/learn/springcloud/priceservice/PriceServiceApplication.java
+++ src/main/java/com/chris/learn/springcloud/priceservice/PriceServiceApplication.java
@@ -2,8 +2,10 @@ package com.chris.learn.springcloud.priceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PriceServiceApplication {
 
 	public static void main(String[] args) {
